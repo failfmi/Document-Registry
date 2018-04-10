94657f866ab5be1157821c0d321114293b773c26
failfmi <fifty_cen7@abv.bg>
2018-04-10 20:07:21 +0300

    Update document-registry.js

--- document-registry.js
+++ document-registry.js
@@ -74,7 +74,7 @@ $(document).ready(function () {
         showView("viewSubmitDocument")
     });
     $('#linkGetDocuments').click(function () {
-		$('#viewGetDocuments div').remove();
+	$('#viewGetDocuments div').remove();
         showView("viewGetDocuments");
         viewGetDocuments();
     });
